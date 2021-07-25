65d43610fafe4e8ea26a4d2ae38cee9772f58aed
Henrique <henrique@falconer.com.br>
2021-07-25 16:57:06 -0300

    fix: remove duplicate CalculatorScreen

--- src/pages/CalculatorScreen/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-import {SafeAreaView} from 'react-native';
-
-export const Container = styled(SafeAreaView)`
-  flex: 1;
-  background-color: #000;
-  justify-content: flex-end;
-`;
