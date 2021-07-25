816072b238039694f276e20e7fb466d5ad2ad7fe
Henrique <henrique@falconer.com.br>
2021-07-25 17:15:18 -0300

    fix: refactor index & CalculatorScreen

--- src/CalculatorScreen.tsx
+++ src/CalculatorScreen.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import ButtonsArea from './components/ButtonsArea';
-import InfoArea from './components/InfoArea';
-import {useOrientation} from './hooks/useOrientation';
+import React from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import ButtonsArea from "./components/ButtonsArea";
+import InfoArea from "./components/InfoArea";
+import { useOrientation } from "./hooks/useOrientation";
 
 const CalculatorScreen: React.FC = () => {
-  const {isPortrait} = useOrientation();
+  const { isPortrait } = useOrientation();
 
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        backgroundColor: '#000',
-        justifyContent: 'flex-end',
-      }}>
+        backgroundColor: "#000",
+        justifyContent: "flex-end",
+      }}
+    >
       <StatusBar barStyle="light-content" />
       <InfoArea />
       {isPortrait && <ButtonsArea />}
