c0051f782d74d18c233f7561858a9c7abd115801
Henrique <henrique@falconer.com.br>
2021-07-25 17:28:10 -0300

    fix: last remaining file w/ double quotes

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,16 +1,16 @@
-import * as math from "mathjs";
-import { OperatorNode } from "mathjs";
-import { Operator } from "./constants";
+import * as math from 'mathjs';
+import { OperatorNode } from 'mathjs';
+import { Operator } from './constants';
 import {
   superscript,
   stringifySymbol,
   unicodeDiagonalFraction,
-} from "./string";
+} from './string';
 
 interface Options {
-  parenthesis?: "keep" | "auto" | "all";
+  parenthesis?: 'keep' | 'auto' | 'all';
   handler?(node: math.MathNode, options: Options): string | void;
-  implicit?: "hide" | "show";
+  implicit?: 'hide' | 'show';
 }
 
 export const isConstant = (
@@ -19,17 +19,17 @@ export const isConstant = (
 ) =>
   (strict
     ? node?.isConstantNode
-    : node && ["SymbolNode", "ConstantNode"].includes(node.type)) ||
+    : node && ['SymbolNode', 'ConstantNode'].includes(node.type)) ||
   (node?.isOperatorNode &&
-    node.fn === "unaryMinus" &&
+    node.fn === 'unaryMinus' &&
     node.args &&
-    ["SymbolNode", "ConstantNode"].includes(node.args[0].type));
+    ['SymbolNode', 'ConstantNode'].includes(node.args[0].type));
 
 export const condPrth = (
   node: math.MathNode,
   str: string,
   ops: string,
-  constArgOps = ""
+  constArgOps = ''
 ) =>
   node.isOperatorNode &&
   node.args?.length === 2 &&
@@ -50,7 +50,7 @@ const commentStringify = (node: math.MathNode | null) => {
   const handler = (node: math.MathNode) => {
     if (node.comment) return node.comment;
   };
-  return node?.toString({ handler }) ?? "";
+  return node?.toString({ handler }) ?? '';
 };
 
 export const splitNumsAndVars = (
@@ -58,24 +58,24 @@ export const splitNumsAndVars = (
   stringifiedNode?: string
 ): [nums: string, vars: math.MathNode | null] => {
   const str = stringifiedNode ?? commentStringify(node);
-  if (!str.match(/^[\d#]/)) return ["", node];
-  const sep = str.indexOf("*");
+  if (!str.match(/^[\d#]/)) return ['', node];
+  const sep = str.indexOf('*');
   if (sep === -1) return [str, null];
   const [nums, vars] = [str.substring(0, sep - 1), str.substring(sep + 2)];
   return [nums, math.parse(vars)];
 };
 
 const options: Options = {
-  parenthesis: "auto",
+  parenthesis: 'auto',
   handler: (node, options) => {
-    if (node.comment) return node.comment.substring(1).replace(".", ",");
+    if (node.comment) return node.comment.substring(1).replace('.', ',');
 
     if (isConstant(node, true))
       return `${
-        node.fn === "unaryMinus"
-          ? "-" + (node.args?.[0].value ?? node.args?.[0].name)
+        node.fn === 'unaryMinus'
+          ? '-' + (node.args?.[0].value ?? node.args?.[0].name)
           : node.value
-      }`.replace(".", ",");
+      }`.replace('.', ',');
 
     if (node.isSymbolNode) return stringifySymbol(node.name);
 
@@ -84,79 +84,79 @@ const options: Options = {
     const [arg1, arg2] = node.args.map((arg) => arg.toString(options));
 
     switch (node.fn) {
-      case "divide":
+      case 'divide':
         if (node.args.every((a) => isConstant(a, true)))
           return unicodeDiagonalFraction(arg1, arg2);
         return (
-          condPrth(node.args[0], arg1, "+-/") +
-          "/" +
-          condPrth(node.args[1], arg2, "+-/*")
+          condPrth(node.args[0], arg1, '+-/') +
+          '/' +
+          condPrth(node.args[1], arg2, '+-/*')
         );
-      case "multiply":
+      case 'multiply':
         const [arg1Prth, arg2Prth] = [arg1, arg2].map((a, i) =>
-          condPrth(node.args[i], a, "+-/", "/")
+          condPrth(node.args[i], a, '+-/', '/')
         );
         return (
           arg1Prth +
-          (arg1.match(/^[\d-×]*$/) && arg2Prth.startsWith("(") ? "×" : "") +
+          (arg1.match(/^[\d-×]*$/) && arg2Prth.startsWith('(') ? '×' : '') +
           arg2Prth
         );
-      case "pow":
-        return condPrth(node.args[0], arg1, "+-/") + superscript(arg2);
-      case "unaryMinus":
-        return "-" + condPrth(node.args[0], arg1, "+-");
+      case 'pow':
+        return condPrth(node.args[0], arg1, '+-/') + superscript(arg2);
+      case 'unaryMinus':
+        return '-' + condPrth(node.args[0], arg1, '+-');
     }
   },
-  implicit: "hide",
+  implicit: 'hide',
 };
 
 export const stringify = (node: math.MathNode) => node.toString(options);
 
 const rules = [
   // Distributivas:
-  "n*(n1+n2) -> (n*n1+n*n2)",
-  "n*(n1-n2) -> (n*n1-n*n2)",
-  "-n*(n1+n2) -> -(n*n1+n*n2)",
-  "-n*(n1-n2) -> -(n*n1-n*n2)",
+  'n*(n1+n2) -> (n*n1+n*n2)',
+  'n*(n1-n2) -> (n*n1-n*n2)',
+  '-n*(n1+n2) -> -(n*n1+n*n2)',
+  '-n*(n1-n2) -> -(n*n1-n*n2)',
   // Outras:
-  "n1/n2*n3 -> n1*n3/n2",
-  "(n1/n2)^c -> n1^c/n2^c",
+  'n1/n2*n3 -> n1*n3/n2',
+  '(n1/n2)^c -> n1^c/n2^c',
   ...math.simplify.rules,
-  "-(n) -> (-n)",
-  "n1+-(n2) -> n1-n2",
-  "n1+-n2 -> n1-n2",
-  "v/c -> 1/c*v",
-  "v*c1/c2 -> c1/c2*v",
-  "v*-c1/c2 -> -c1/c2*v",
-  "v*c -> c*v",
-  "v*-c -> -c*v",
-  "c1*(1/c2) -> c1/c2",
-  "-c1*(1/c2) -> -c1/c2",
-  "v1/v2*v3 -> v1*v3/v2",
+  '-(n) -> (-n)',
+  'n1+-(n2) -> n1-n2',
+  'n1+-n2 -> n1-n2',
+  'v/c -> 1/c*v',
+  'v*c1/c2 -> c1/c2*v',
+  'v*-c1/c2 -> -c1/c2*v',
+  'v*c -> c*v',
+  'v*-c -> -c*v',
+  'c1*(1/c2) -> c1/c2',
+  '-c1*(1/c2) -> -c1/c2',
+  'v1/v2*v3 -> v1*v3/v2',
 ];
 
 const postInversionRules = [
   // Junção de numeradores para a inversão:
-  "n1/n+n3/n -> (n1+n3)/n",
-  "n1/n-n3/n -> (n1-n3)/n",
-  "-(n1/n)+n3/n -> (-n1+n3)/n",
-  "-(n1/n)-n3/n -> (-n1-n3)/n",
-  "n1*n2/n+n3*n4/n -> (n1*n2+n3*n4)/n",
-  "n1*n2/n+n3/n -> (n1*n2+n3)/n",
-  "-(n1*n2/n)+n3*n4/n -> (-n1*n2+n3*n4)/n",
-  "-(n1*n2/n)+n3/n -> (-n1*n2+n3)/n",
-  "n1*n2/n-n3*n4/n -> (n1*n2-n3*n4)/n",
-  "n1*n2/n-n3/n -> (n1*n2-n3)/n",
-  "-(n1*n2/n)-n3*n4/n -> (-n1*n2-n3*n4)/n",
-  "-(n1*n2/n)-n3/n -> (-n1*n2-n3)/n",
+  'n1/n+n3/n -> (n1+n3)/n',
+  'n1/n-n3/n -> (n1-n3)/n',
+  '-(n1/n)+n3/n -> (-n1+n3)/n',
+  '-(n1/n)-n3/n -> (-n1-n3)/n',
+  'n1*n2/n+n3*n4/n -> (n1*n2+n3*n4)/n',
+  'n1*n2/n+n3/n -> (n1*n2+n3)/n',
+  '-(n1*n2/n)+n3*n4/n -> (-n1*n2+n3*n4)/n',
+  '-(n1*n2/n)+n3/n -> (-n1*n2+n3)/n',
+  'n1*n2/n-n3*n4/n -> (n1*n2-n3*n4)/n',
+  'n1*n2/n-n3/n -> (n1*n2-n3)/n',
+  '-(n1*n2/n)-n3*n4/n -> (-n1*n2-n3*n4)/n',
+  '-(n1*n2/n)-n3/n -> (-n1*n2-n3)/n',
   // Regras básicas
   ...math.simplify.rules,
-  "-(n) -> (-n)",
-  "n1+-(n2) -> n1-n2",
-  "n1+-n2 -> n1-n2",
+  '-(n) -> (-n)',
+  'n1+-(n2) -> n1-n2',
+  'n1+-n2 -> n1-n2',
 ];
 
-const simpleInversionRules = ["1/(1/n) -> n", "1/(-1/n) -> -n"];
+const simpleInversionRules = ['1/(1/n) -> n', '1/(-1/n) -> -n'];
 
 export const simplify = (node: math.MathNode) => math.simplify(node, rules);
 export const simplifyInversion = (node: math.MathNode) =>
@@ -166,10 +166,10 @@ const buildOperation = (symbol: string, symbolName: string) => (
   ...elems: math.MathNode[]
 ) => new OperatorNode(symbol, symbolName, elems) as math.MathNode;
 
-export const add = buildOperation("+", "add");
-export const subtract = buildOperation("-", "subtract");
-export const multiply = buildOperation("*", "multiply");
-export const divide = buildOperation("/", "divide");
+export const add = buildOperation('+', 'add');
+export const subtract = buildOperation('-', 'subtract');
+export const multiply = buildOperation('*', 'multiply');
+export const divide = buildOperation('/', 'divide');
 
 export const genericOperation = (
   operator: Operator,
@@ -185,12 +185,12 @@ export const genericOperation = (
     case Operator.Divide:
       return divide(...elems);
   }
-  throw "Error: Invalid Operator";
+  throw 'Error: Invalid Operator';
 };
 
-export const zero = math.parse("0");
-export const plusOne = math.parse("1");
-export const minusOne = math.parse("-1");
+export const zero = math.parse('0');
+export const plusOne = math.parse('1');
+export const minusOne = math.parse('-1');
 
 export const condNeg = (node: math.MathNode, neg: boolean) =>
   neg ? multiply(minusOne, node) : node;
@@ -200,7 +200,7 @@ export const parseComma = (node: math.MathNode) => {
   if (!str.match(/#/)) return node;
   const [nums, vars] = splitNumsAndVars(node, str);
   const newNums = math.simplify(
-    math.parse(nums.substring(1).replace(/\.$/, ""))
+    math.parse(nums.substring(1).replace(/\.$/, ''))
   );
   return vars ? multiply(newNums, vars) : newNums;
 };
