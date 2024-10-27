80efba75383ec330e2e128154ffd0413f06e62aa
Henrique <henrique@falconer.com.br>
2024-10-27 18:52:47 -0300

    🎗️ add TODO to fix app icon

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./src";
+import { name as appName } from "./app.json";
+
+// TODO: Make app icon work again in iOS (after updating to react-native v0.76, the app icon stopped working for some reason)
 
 AppRegistry.registerComponent(appName, () => App);
